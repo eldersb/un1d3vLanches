9eb4b7cabb7dc746d0dfc605f8ab9053c5251f9b
eldersb <122701368+eldersb@users.noreply.github.com>
2024-05-23 23:15:03 -0300

    Tela de carrinho

--- src/components/HeaderComponente.vue
+++ src/components/HeaderComponente.vue
@@ -3,13 +3,17 @@
 
         <div class="container">
 
-            <nav class="d-flex justify-content-center align-items-center mt-3 mb-3 gap-2">
-            <img src="../assets/logo-hamburguer.png" alt="...">
-            <div>
-              <h1>UN1D3V Lanches</h1>
-              <p>Código e Lanches</p>
+            <nav class="d-flex justify-content-between align-items-center mt-3 mb-3 gap-2">
+              <div class="d-flex align-items-center">
+
+               <a href="/"><img  src="../assets/logo-hamburguer.png" alt="..."></a> 
+                <div class="ms-4">
+                  <h1 class="mb-0">UN1D3V Lanches</h1>
+                  <p class="mb-0">Código e Lanches</p>
+                </div>
               </div>
-            </nav>
+                <a href="/about"  class="btn ml-5"><img class="icone-carrinho" src="../assets/carrinho.png" alt="icone carinho"></a> 
+              </nav>
         </div>
 
     </header>
@@ -30,6 +34,11 @@ header {
 h1 {
   color: #D42401;
   font-size: 28px
+}
+
+.icone-carrinho {
+  width: 32px;
+  height: 32px;
 }
 
 </style>
--- src/views/CarrinhoView.vue
+++ src/views/CarrinhoView.vue
@@ -1,6 +1,55 @@
 <template>
     <div class="about">
-      <h1>This is an about page</h1>
+      <h1 class="text-center mt-5">Resumo do Pedido</h1>
+
+
+      <div class="container">
+        <div class="row d-flex justify-content-center mt-5">
+
+          <div class="card text-center mb-3" style="width: 40rem;">
+             <div class="card-body">
+             <h5 class="card-title">Resumo do pedido</h5>
+             <hr>
+             <div class="div d-flex justify-content-between">
+               <p class="card-text">1x BACON MELT CATUPIRY</p>
+               <div class="div d-flex">
+                 <p>22,99</p>
+                 <img class="ms-5" src="" alt="icone-lixeira">
+
+               </div>
+             </div>
+             <hr>
+             <a href="#" class="btn">adicionar itens</a>
+             <hr>
+             <div class="div">
+               <p>Subtotal</p>
+               <p>R$ 22,99</p>
+
+             </div>
+             <hr>
+             <a href="#" class="btn">Cupom de Desconto</a>
+      </div>
+    </div>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-center mt-5">
+
+        <a href="#" class="btn ">Ver opções de entrega</a>
+      </div>
+
+      
     </div>
   </template>
+
+
+<style>
+
+.btn {
+  background-color: #D42401 ;
+  color: white;
+}
+
+
+</style>
   
